e4d748d156e6921b441d1829d32320c160a4f9e6
Tobias Diez <code@tobiasdiez.de>
2025-06-02 18:14:22 +0800

    Improve build script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,14 @@
 
 :: Following https://sourceforge.net/p/maxima/code/ci/master/tree/INSTALL.lisp
 
-:: Build
-ecl -shell build-ecl.lisp
+:: Configure
+ecl -load configure.lisp -eval "(configure :interactive nil)" -eval "(quit)"
 if errorlevel 1 exit 1
+
+:: Build
+cd src
+ecl --load maxima-build.lisp -eval "(require 'cmp)" -eval "(maxima-compile)" -eval "(quit)"
+if errorlevel 1 exit 1
+
+:: Test
+:: TODO
